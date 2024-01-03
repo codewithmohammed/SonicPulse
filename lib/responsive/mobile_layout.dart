85b5a23ad74c1a821352438a0a609ad2e0d1153b
*Mohammed <rayidrasal@gmail.com>
2024-01-03 21:37:01 +0400

    the appbar issue is over. the carousel is placed correctly and the there is some adjustment to make in text size of the appbar

--- lib/responsive/mobile_layout.dart
+++ lib/responsive/mobile_layout.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:musicplayer/responsive/dimensions.dart';
+import 'package:musicplayer/utils/tab.dart';
 import 'package:svg_flutter/svg.dart';
 
 class MobileLayoutScreen extends StatelessWidget {
@@ -9,57 +11,86 @@ class MobileLayoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final mheight =
+        DimensionUtil.findDimension(context: context, isHeight: true);
+    final mwidth =
+        DimensionUtil.findDimension(context: context, isHeight: false);
     return Scaffold(
-        backgroundColor: colorScheme.primary,
-        appBar: AppBar(
-            backgroundColor: colorScheme.background,
-            leadingWidth: 200,
-            leading: Row(
-              children: [
-                SvgPicture.asset(
-                  'assets/images/SONIC.svg',
-                  height: 15,
-                  colorFilter:
-                      ColorFilter.mode(colorScheme.secondary, BlendMode.srcIn),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  'Pulse',
-                  style: GoogleFonts.kolkerBrush(
-                      textStyle: const TextStyle(fontSize: 28)),
-                ),
-              ],
+        backgroundColor: colorScheme.background,
+        body: Stack(
+          fit: StackFit.loose,
+          children: [
+            SizedBox(
+              // height: 180,
+              child: Column(
+                children: [
+                  AppBar(
+                    backgroundColor: colorScheme.background,
+                    leadingWidth: 200,
+                    leading: Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/SONIC.svg',
+                            height: 12,
+                            colorFilter: ColorFilter.mode(
+                                colorScheme.secondary, BlendMode.srcIn),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            'Pulse',
+                            style: GoogleFonts.kolkerBrush(
+                                textStyle: const TextStyle(fontSize: 25)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    actions: [
+                      IconButton(
+                          tooltip: 'Search',
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.search,
+                            semanticLabel: 'Search',
+                          )),
+                      IconButton(
+                          tooltip: 'More Option',
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.more_vert,
+                            semanticLabel: 'More Option',
+                          )),
+                    ],
+                    // bottom: const CustomAppBar(),
+                  ),
+                  const CustomAppBar()
+                ],
+              ),
             ),
-            actions: [
-              IconButton(
-                  tooltip: 'Search',
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                    semanticLabel: 'Search',
-                  )),
-              IconButton(
-                  tooltip: 'More Option',
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.more_vert,
-                    semanticLabel: 'More Option',
-                  )),
-            ],
-          bottom: CustomAppBar(),
-          ),
-        body: SafeArea(
-            child: Stack(children: [
-          
-          Container(child: Container(),)
-        ],
-        
-        
-        ),
-        
-        
+            Positioned(
+              top: 160,
+              child: Container(
+                height: mheight,
+                width: mwidth,
+                decoration: BoxDecoration(
+                    color: colorScheme.primary,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: [
+                      BoxShadow(
+                          color: colorScheme.tertiary,
+                          offset: const Offset(0, 13),
+                          blurRadius: 20,
+                          blurStyle: BlurStyle.outer)
+                    ]),
+
+                // child: const Text('hello'),
+                // child: TabBarView(children: tabsContent),
+              ),
+            )
+          ],
         ));
   }
 }
@@ -74,181 +105,74 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      color: colorScheme.background,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-child:
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: [
-            //     Row(
-            //       children: [
-            //         SvgPicture.asset(
-            //           'assets/images/SONIC.svg',
-            //           height: 20,
-            //           colorFilter: ColorFilter.mode(
-            //               colorScheme.secondary, BlendMode.srcIn),
-            //         ),
-            //         const SizedBox(
-            //           width: 5,
-            //         ),
-            //         Text(
-            //           'Pulse',
-            //           style: GoogleFonts.kolkerBrush(
-            //               textStyle: const TextStyle(fontSize: 35)),
-            //         ),
-            //       ],
-            //     ),
-            //     Row(
-            //       children: [
-            //         IconButton(
-            //             tooltip: 'Search',
-            //             onPressed: () {},
-            //             icon: const Icon(
-            //               Icons.search,
-            //               semanticLabel: 'Search',
-            //             )),
-            //         IconButton(
-            //             tooltip: 'More Option',
-            //             onPressed: () {},
-            //             icon: const Icon(
-            //               Icons.more_vert,
-            //               semanticLabel: 'More Option',
-            //             )),
-            //       ],
-            //     )
-            //   ],
-            // ),
-            CarouselSlider(
-                items: [
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                            fontSize: 18, color: colorScheme.secondary),
-                      )),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: colorScheme.secondary,
-                        ),
-                      )),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                            fontSize: 18, color: colorScheme.secondary),
-                      )),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                            fontSize: 18, color: colorScheme.secondary),
-                      )),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                            fontSize: 18, color: colorScheme.secondary),
-                      )),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                            fontSize: 18, color: colorScheme.secondary),
-                      )),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Spotify",
-                        style: TextStyle(
-                            fontSize: 18, color: colorScheme.secondary),
-                      )),
-                ],
-                options: CarouselOptions(
-                    padEnds: true,
-                    height: 30,
-                    pageSnapping: true,
-                    enableInfiniteScroll: false,
-                    enlargeCenterPage: true,
-                    enlargeFactor: 0.3,
-                    viewportFraction: 0.26,
-                    enlargeStrategy: CenterPageEnlargeStrategy.zoom))
-
-      ),
-    );
+    final mwidth =
+        DimensionUtil.findDimension(context: context, isHeight: false);
+    double textSize = mwidth * 0.05;
+    return CarouselSlider(
+        items: [
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                    fontSize: textSize, color: colorScheme.secondary),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                  fontSize: textSize,
+                  color: colorScheme.secondary,
+                ),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                    fontSize: textSize, color: colorScheme.secondary),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                    fontSize: textSize, color: colorScheme.secondary),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                    fontSize: textSize, color: colorScheme.secondary),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                    fontSize: textSize, color: colorScheme.secondary),
+              )),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "Spotify",
+                style: TextStyle(
+                    fontSize: textSize, color: colorScheme.secondary),
+              )),
+        ],
+        options: CarouselOptions(
+            padEnds: true,
+            height: 50,
+            pageSnapping: true,
+            enableInfiniteScroll: false,
+            enlargeCenterPage: true,
+            enlargeFactor: 0.3,
+            viewportFraction: 0.26,
+            enlargeStrategy: CenterPageEnlargeStrategy.zoom));
   }
 
   @override
-  Size get preferredSize => const Size(double.maxFinite, 30);
+  Size get preferredSize => const Size(double.maxFinite, 0);
 }
-
-// DefaultTabController(
-//       length: 7,
-//       child: Scaffold(
-//         backgroundColor: colorScheme.background,
-//         appBar: AppBar(
-//           backgroundColor: colorScheme.background,
-//           leading: Row(
-//             children: [
-//               const SizedBox(
-//                 width: 10,
-//               ),
-//               SvgPicture.asset(sonicSvg,
-//                   height: 13,
-//                   colorFilter:
-//                       ColorFilter.mode(colorScheme.secondary, BlendMode.srcIn)),
-//               const SizedBox(
-//                 width: 10,
-//               ),
-//               Text('Pulse',
-//                   style: GoogleFonts.kolkerBrush(
-//                       fontSize: 30, color: colorScheme.secondary))
-//             ],
-//           ),
-//           leadingWidth: mWidth * 0.5,
-//           actions: [
-//             IconButton(
-//               onPressed: () {},
-//               icon: Icon(
-//                 Icons.search,
-//                 color: colorScheme.secondary,
-//               ),
-//               tooltip: 'Search',
-//               mouseCursor: MaterialStateMouseCursor.clickable,
-//             ),
-//             IconButton(
-//               onPressed: () {},
-//               icon: Icon(
-//                 Icons.more_vert,
-//                 color: colorScheme.secondary,
-//               ),
-//               tooltip: 'More Options',
-//               mouseCursor: MaterialStateMouseCursor.clickable,
-//             ),
-//           ],
-//           bottom: PreferredSize(
-//             preferredSize: const Size.fromHeight(50),
-//             child: GestureDetector(
-//               dragStartBehavior: DragStartBehavior.start,
-//               child: TabBar(
-//                 tabAlignment: TabAlignment.start,
-//                 isScrollable: true,
-//                 tabs: tabs,
-//                 padding:  EdgeInsets.only(left: mWidth * 0.4, right: mWidth * 0.4),
-//               ),
-//             ),
-//           ),
-//         ),
-//         body: TabBarView(children: tabsContent),
-//       ),
-//     );
